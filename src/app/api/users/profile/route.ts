0f3a118e2202b48bc51c73d897d0b3c817a4d47e
Manomay <bmanomay2018@gmail.com>
2025-08-13 18:50:35 +0530

    Final Base Structure

--- src/app/api/users/profile/route.ts
+++ src/app/api/users/profile/route.ts
@@ -1,6 +1,5 @@
 import {connect} from "@/dbConfig/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
-import Playlist from "@/models/playlistModel";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 
 connect()
